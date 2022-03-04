553da2781a6a3f4fc5cc6620545ce9e70e2aea48
aditya <aditya.1923it1006@kiet.edu>
2022-03-05 02:28:15 +0530

    first_commit

---
+++ video1&2/first_program.cpp
@@ -0,0 +1,22 @@
+//C++ language was developed in 1979 by bjarne stroustrup
+//Fast program , more control over the system resources , memory management
+//High Performance
+//Major updates of C++ :
+    //2011 -> C++ 11
+    //2014 -> C++ 14
+    //2017 -> C++ 17
+
+//Basic structure of C++ program
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Hello World!";
+    //std::cout<<"Hello World!";
+    return 0;
+}
+
+//#include<iostream> -> Header File(increase the program speed ) -> It helps in input /output -> cin ,cout -> More functions are there
+//int main() -> int is the data type and main is the function from where the program begins -> program always starts from the main
+// To not use std::cout at every line we just write -> using namespace std;
+//return 0 means successful termination of the program
---
+++ video3/Variables_comments.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum = 6;
+    cout<<"Hello World " << sum;
+    return 0;
+}
+
+
+//Low level language ->  hardware k pass
+//high level language -> Logically sound and hardware se dur
+//C++ is a low level language
+//Everything is proceesed in numbers
+
+
+//Variables:
+    //Containers that stores the data
+    //Data types -> int , float , char 
+
+//to write multi line comments
+ /* 
+ hello i am multi line comment
+ aditya saini
+ */
