4a0f537e900232e5fa648595aab2d023d5055580
aditya <aditya.1923it1006@kiet.edu>
2022-03-30 01:31:24 +0530

    commit

--- video4/local_global.cpp
+++ video4/local_global.cpp
@@ -13,7 +13,7 @@ int main(){
     bool is_true = true; //prints 1 in case if True and 0 for False
     int glo = 9;  //Yes , we can declare global and local variable with the same name
     glo = 78;  //updates the local variable
-    cout<<"\nThe value of glo is: "<<glo <<is_true; //This will print 9
+    cout<<"\nThe value of glo is: "<<glo <<is_true; //This will print 78
     return 0;
 }
 
